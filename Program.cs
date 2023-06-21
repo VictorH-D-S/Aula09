dca0ef8a5165574df38cab2b11b6677fc7009305
Victor H Santos <135677578+VictorH-D-S@users.noreply.github.com>
2023-06-21 03:11:26 -0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,79 +1,50 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Aula09ProdutosEstoques
+namespace Aula09ProdutosEstoqueSegQua
 {
     class Program
     {
-        static List<string> produtos = new List<string>() { "pepsi", "fanta" };
         static void Main(string[] args)
         {
-
-
-            Console.WriteLine("Bem vindo ao Conrado Gamer Store");
-            Console.WriteLine("1- Cadastrar novo produto\n2- Listar produtos existentes");
-            string option = Console.ReadLine();
-            
-            
-            if (option == "1")
+            List<string> produtos = new List<string> { "pepsi", "fanta" };
+            Console.WriteLine("Bem-vindo ao Conradito Store");
+            while (true)
             {
-
-
-                Console.Write("Insira o produto a ser cadastrado: ");
-                string produto = Console.ReadLine();
-
-                if (produtos.Contains(produto))
+                Console.WriteLine("Digite 1 para cadastrar ou 2 para listar");
+                string resposta = Console.ReadLine();
+                if (resposta == "1")
                 {
-
-                    Console.WriteLine("Produto já cadastrado...");
-                    Console.ReadKey();
-                    Console.Clear();
-                    Main(args);
-                    return;
-
+                    Console.WriteLine("Digite o nome do produto");
+                    string produto = Console.ReadLine();
+                    produto = produto.ToLower();
+                    if (produtos.Contains(produto))
+                    {
+                        Console.WriteLine("Esse produto já está na lista...");
+                    }
+                    else
+                    {
+                        produtos.Add(produto);
+                        Console.WriteLine("Produto adicionado!");
+                    }
                 }
                 else
                 {
-                    produtos.Add(produto);
-                    Console.WriteLine($"O item {produto} foi adicionado com sucesso!");
-                    Console.ReadKey();
-                    Console.Clear();
-                    Main(args);
-                    return;
-                }
-            }
-
-            else
-            {
-
-                string texto = "";
-                int contador = 0;
-                while (contador < produtos.Count)
-                {
-
-                    string nome = produtos[contador];
-                    string primeira_letra = nome.Substring(0, 1).ToUpper();
-                    string resto = nome.Substring(1, nome.Length - 1);
-                    string nome_tratado = primeira_letra + resto;
-
-                    texto += nome_tratado + ", ";
-                    contador++;
-
+                    string texto = "";
+                    int contador = 0;
+                    while (contador < produtos.Count)
+                    {
+                        string nome = produtos[contador];
+                        string primeira_letra = nome.Substring(0, 1).ToUpper();
+                        string resto = nome.Substring(1, nome.Length - 1);
+                        string nome_tratado = primeira_letra + resto;
+                        texto += nome_tratado + ", ";
+                        contador++;
+                    }
+                    texto = texto.Substring(0, texto.Length - 2);
+                    Console.WriteLine(texto);
                 }
 
-                texto = texto.Substring(0, 1);
-
-                Console.WriteLine(texto);
-                Console.ReadLine();
-                Console.Clear();
-                Main(args);
-                return;
-
             }
-
         }
     }
 }
